ec2368f6ded8bad5183216aec981769cba986119
Matthew Shaw <mattalexshaw@gmail.com>
2022-10-16 18:30:28 +0100

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="templater",
+    version="0.0.1",
+    description="Tool for formatting and parsing string templates",
+    author="Matthew Shaw",
+    url="https://github.com/MattRickS/templater",
+    package_dir={"": "python"},
+    packages=["templater"],
+)
